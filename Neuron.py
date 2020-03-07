d973fa8a5d52cd40b82fef4684d7059c3acf9f8f
Matthew Dupree <mattjdupree@gmail.com>
2020-03-06 19:17:05 -0800

    Integrated weight update into LIF J stamping

--- Neuron.py
+++ Neuron.py
@@ -58,6 +58,20 @@ class ModelNeuron:
     # def stampNonlinearJ(self,J,vguess)
 
 
+def LIF_weight_update(w, ispike):
+    # W-current weight of a neuron and the function returns the updated weight
+    # ispike is an array of size 5 with input data from time steps t-4 to t
+    if (ispike[1] == 1):
+        w += 2
+    if (ispike[0] == 1):
+        w += 1
+    if (ispike[3] == 1):
+        w -= 2
+    if (ispike[4] == 1):
+        w -= 1
+    return w
+
+
 class LIFNeuron(ModelNeuron):
     def __init__(self, inputNeuronIds, nV):
         super().__init__(inputNeuronIds, nV)
@@ -80,7 +94,8 @@ class LIFNeuron(ModelNeuron):
 
     def stampCompanionJ(self, J, vlast, slast, stdpSpiked):
         if stdpSpiked:
-            pass  # TODO: STDP Learning weight vector update
+            for i in range(len(self.w)):
+                self.w[i] = LIF_weight_update(self.w[i], slast[i, :])
 
         if vlast > LIFThreshold:
             vlast = LIFrestingPotential  # Spike reset
--- simulator.py
+++ simulator.py
@@ -72,22 +72,6 @@ def simulator(modelFile, inputFile):
     for n in outputNeurons:
         n.plot(tsteps)
 
-def weight_update(self, w, ispike, ospike):
-    # W-current weight of a neuron and the function returns the updated weight
-    # ispike is an array of size 5 with input data from time steps t-4 to t
-    # ospike is the output data at time step t-2
-    if (ospike == 1):
-        if (ispike[1] == 1):
-            w+= 2
-        if (ispike[0] == 1):
-            w+= 1
-        if (ispike[3] == 1):
-            w-= 2
-        if (ispike[4] == 1):
-            w-= 1
-        return w
-    else:
-        return w
 
 if __name__ == "__main__":
     import sys
